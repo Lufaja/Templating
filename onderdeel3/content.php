16e5997c837c13b1bdfa8047784dfc7f09557ff4
Lukas van Hulst <Lukas.v.hulst@gmail.com>
2023-05-10 11:03:25 +0200

    onderdeel 3 gemaakt

---
+++ onderdeel3/content.php
@@ -0,0 +1,5 @@
+<?php
+$naam = "Lukas van Hulst";
+echo "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui veniam nobis quas earum ea amet pariatur velit? Adipisci odit, eaque quo earum molestias perferendis eos, corrupti quos maxime mollitia quas!";
+echo '<img src="https://cdn-production-opera-website.operacdn.com/staticfiles/assets/images/pages/client/welcomeGx/opera-gx-h-logo.d0c851c56674.svg">';
+?>
